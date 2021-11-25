c6a3462e11ba730e65a9434f9079c6ede7ecabdb
MiDo1806 <81757415+MiDo1806@users.noreply.github.com>
2021-11-25 11:26:33 +0100

    Add files via upload

---
+++ JavaScript opdracht3/script.js
@@ -0,0 +1,35 @@
+const age = 18;
+if (age <= 18) {
+
+    console.log("You are more than welcome to stay");
+}
+
+else {
+
+    console.log("then i will ask you politely to leave");
+
+}
+
+const isFemale = true;
+if (isFemale) {
+
+    console.log("You are female");
+
+}
+
+else {
+
+    console.log("You are not female")
+}
+
+const driverStatus = true;
+
+if (driverStatus) {
+
+    console.log("You are the Bob");
+}
+
+else {
+
+    console.log("Call a taxi")
+}
